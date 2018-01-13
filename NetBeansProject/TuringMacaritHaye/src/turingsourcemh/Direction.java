600f553b625ad8bb3b6d432482064d8194ab96f5
JLMacarit <jl.macarit@ymail.com>
2018-01-13 18:55:40 +0100

    implémentageage modèle,début

---
+++ NetBeansProject/TuringMacaritHaye/src/turingsourcemh/Direction.java
@@ -0,0 +1,8 @@
+package turingsourcemh;
+
+//enumeration des directions possibles pour la tete de lecture
+public enum Direction {
+    dr, // droite
+    ga, // gauche
+    pm // pas de mouvement
+}
--- NetBeansProject/TuringMacaritHaye/src/turingsourcemh/ModTuring.java
+++ NetBeansProject/TuringMacaritHaye/src/turingsourcemh/ModTuring.java
@@ -5,32 +5,34 @@ import java.util.ArrayList;
 /**
  *
  * @author Jean-Loup
- */
+ */    
 public class ModTuring {
 
     private boolean arret;
     private TabTuRegles tabRegles;
     private int etatCourant;
     private Character[] ruban;
+    private int posTete; //position de la tete de lecture
     
-    public enum Direction {
-        dr,
-        ga,
-        fi
-    }
-
     public ModTuring() {
-
+        this.arret=false;
+        this.etatCourant=0;
+        this.tabRegles=new TabTuRegles();
+        this.ruban=new Character[0];
+        this.posTete=0;
     }
 
+    // règles 
     public class TuRegle {
 
+        //attrbuts
         private int ec; //etat courant
         private Character sl; //symbole lu
         private int es; // etat suivant
         private Character se; // symbole écrit
         private Direction di; // direction de la tete de lecture
         
+        //constructeur basique
         public TuRegle (int ec, Character sl, int es, Character se, Direction di){
             this.ec = ec;
             this.sl = sl;
@@ -38,15 +40,38 @@ public class ModTuring {
             this.se = se;
             this.di = di;
         }
+        
+        //methode de verification si la regle correspond
+        public boolean verif(int ec, Character sl){
+            return (this.ec == ec && this.sl == sl);
+        }
+        
+        //getters pour l'application de la regle
+        public int etatSuiv(){
+            return this.es;
+        }
+        
+        public Character symbEcrit(){
+            return this.se;
+        }
+        
+        public Direction dirTete(){
+            return this.di;
+        }
 
     }
 
+    // collection de règles
     public class TabTuRegles {
 
-        private ArrayList<TuRegle> tabTuRegle;
+        private ArrayList<TuRegle> tabTuRegles;
 
         public TabTuRegles() {
-
+            tabTuRegles = new ArrayList<TuRegle>();
+        }
+        
+        public void ajoutRegle(TuRegle tr){
+            tabTuRegles.add(tr);
         }
 
     }
