e1448cfd66ca065e98e666b589c7b7f67cc9bb21
JLMacarit <jl.macarit@ymail.com>
2018-01-13 17:00:49 +0100

    début proto modèle

--- NetBeansProject/TuringMacaritHaye/src/turingsourcemh/ModTuring.java
+++ NetBeansProject/TuringMacaritHaye/src/turingsourcemh/ModTuring.java
@@ -1,14 +1,54 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package turingsourcemh;
+
+import java.util.ArrayList;
 
 /**
  *
  * @author Jean-Loup
  */
 public class ModTuring {
+
+    private boolean arret;
+    private TabTuRegles tabRegles;
+    private int etatCourant;
+    private Character[] ruban;
     
+    public enum Direction {
+        dr,
+        ga,
+        fi
+    }
+
+    public ModTuring() {
+
+    }
+
+    public class TuRegle {
+
+        private int ec; //etat courant
+        private Character sl; //symbole lu
+        private int es; // etat suivant
+        private Character se; // symbole écrit
+        private Direction di; // direction de la tete de lecture
+        
+        public TuRegle (int ec, Character sl, int es, Character se, Direction di){
+            this.ec = ec;
+            this.sl = sl;
+            this.es = es;
+            this.se = se;
+            this.di = di;
+        }
+
+    }
+
+    public class TabTuRegles {
+
+        private ArrayList<TuRegle> tabTuRegle;
+
+        public TabTuRegles() {
+
+        }
+
+    }
+
 }
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 gitkraken-arthjihel
+
+Ce projet vise à modéliser en java une machine de Turing.
+Elle va connaître une tripotée d'updates dans la journée.
+Elle est réalisée par Arthur HAYE et Jean-Loup MACARIT suivant le pattern MVC.
+
+Objectifs : 
+1)
+	- prototyper le modèle, les règles et la vue
+		- modèle :
+			implémenter la sous-classe TuRegle
+			implémenter l'ajout et la suppression de regle
+			implémenter la sous-classe sérialisable TabTuRegles
+2)
+	- implémenter le modèle
+3)
+	- prototyper le contrôleur
+4) 
+	- implémenter le contrôleur
+5) 
+	- implémenter la vue
+
+V0.0 :
+Objectif : 
