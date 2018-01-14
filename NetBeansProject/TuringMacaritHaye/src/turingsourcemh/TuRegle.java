ce534e1e9904872c8d7ac0b3bb976c6cbf8bf32e
JLMacarit <jl.macarit@ymail.com>
2018-01-14 23:40:37 +0100

    faireunpascamarcheetdemarreraussi

--- NetBeansProject/TuringMacaritHaye/src/turingsourcemh/TuRegle.java
+++ NetBeansProject/TuringMacaritHaye/src/turingsourcemh/TuRegle.java
@@ -30,8 +30,14 @@ public class TuRegle implements Serializable, Cloneable {
     }
 
     //methode de verification si la regle correspond
-    public boolean verif(int ec, Character sl) {
-        return (this.ec == ec && this.sl == sl);
+    public boolean verif(int ecp, Character slp) {
+        System.out.println("verif : "+this+"avec ec:"+ecp+"/"+this.ec+" et sl:"+slp+"/"+this.sl);
+        if ((this.ec == (ecp)) && (this.sl.compareTo(slp) == 0)){
+            System.out.println("ok y a une regle");
+            return true;
+        } else {
+            return false;
+        }
     }
 
     //getters pour l'application de la regle
