490c11081e9fa2798312a2753bcb5f5c324fbd09
JLMacarit <jl.macarit@ymail.com>
2018-01-14 02:34:23 +0100

    presque fini modele

--- NetBeansProject/TuringMacaritHaye/src/turingsourcemh/ModTuring.java
+++ NetBeansProject/TuringMacaritHaye/src/turingsourcemh/ModTuring.java
@@ -1,6 +1,6 @@
 package turingsourcemh;
 
-import java.io.Serializable;
+//import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
@@ -18,7 +18,7 @@ public class ModTuring {
     public ModTuring() {
         this.arret = false;
         this.etatCourant = 0;
-        this.tabRegles = new ArrayList<TuRegle>();
+        this.tabRegles = new ArrayList<>();
         this.ruban = new Character[0];
         this.posTete = 0;
     }
@@ -31,6 +31,10 @@ public class ModTuring {
         return this.posTete;
     }
 
+    public boolean arretee(){
+        return this.arret;
+    }
+    
     // ajout regle
     public void ajoutTuRegle(int ec, Character sl, int es, Character se, Direction di) {
         TuRegle r = new TuRegle(ec, sl, es, se, di);
@@ -42,6 +46,79 @@ public class ModTuring {
         this.tabRegles.remove(pos);
     }
 
+    // lire symbole
+    public Character lireSymb() {
+        Character lu;
+        try {
+            lu = ruban[posTete];
+        } catch (IndexOutOfBoundsException e) {
+            lu = Character.MIN_VALUE; //caractere vide, mais quand meme caractere, a la difference de null
+        }
+        return lu;
+    }
+
+    //verifier presence d'une regle
+    //retourne l'indice de la regle dans la collection
+    //retourne -1 si pas de regle correspondante
+    public int trouveRegle() {
+        boolean e = false; // voir si regle existe
+        int i = -1;
+        while (!e) {
+            ++i;
+            e = this.tabRegles.get(i).verif(this.etatCourant, lireSymb());
+        }
+        if (e) {
+            return i;
+        } else {
+            return -1;
+        }
+    }
+
+    //ecrire dans le ruban
+    public void ecrireRuban (int pos, Character sym){
+        try{
+            this.ruban[pos] = sym;
+        } catch (IndexOutOfBoundsException e){
+            // nothing
+        }
+    }    
+    
+    //bouger la tete de lecture
+    public void updatePosTete (Direction dir){
+        switch (dir){
+            case dr :
+                ++this.posTete;
+                break;
+            case ga :
+                --this.posTete;
+                break;
+            case pm :
+                //nothing
+                break;
+        }
+    }
+    
+    //appliquer une regle
+    public boolean appliquer() {
+        boolean wentWell = true;
+        int indRegle = trouveRegle();
+        
+        if (indRegle != -1){
+            TuRegle regle = this.tabRegles.get(indRegle);
+            this.etatCourant = regle.etatSuiv();
+            ecrireRuban(this.posTete, regle.symbEcrit());
+            updatePosTete (regle.dirTete());
+        } else {
+            wentWell = false;
+        }     
+        
+        return wentWell;
+    }
+    
+    
+
+    // verifier quand on lit que la case est definie, si elle ne l'est pas, 
+    // faire comme si c'était une case vide (particulierement vrai pour indices negatifs
     /*
     // collection de règles
     public class TabTuRegles implements Serializable, Cloneable {
